cd2ff4d7c2aafae569b235a5b850617169730048
Lucianor01 <lucianorosario02@gmail.com>
2023-06-14 17:00:11 +0200

    Comics Table

--- database/migrations/2023_06_14_131235_create_comics_table.php
+++ database/migrations/2023_06_14_131235_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->nullable();
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->smallInteger('price')->nullable();
+            $table->string('series')->nullable();
+            $table->dateTime('sale_date')->nullable();
+            $table->string('type')->nullable();
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
